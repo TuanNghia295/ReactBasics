60356711ee2ba2b3523986de08ad21840fd54724
TuanNghia295 <tuannghiait2905@gmail.com>
2023-04-15 12:50:59 +0700

    UseState and useEffect

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,62 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index,setIndex] = useState(0)
+  const {name,job,image,text} = people[index]
+
+  const checkNumber = (number) =>{
+    if(number > people.length-1){
+      return 0;
+    }
+    if(number < 0){
+      return people.length-1
+    }
+    return number
+  }
+
+  const handlePrePerson = ()=>{
+    setIndex((pre)=>{
+      let newIndex = pre - 1
+      return  checkNumber(newIndex)
+    })
+  }
+
+  const handleNextPerson = ()=>{
+    setIndex((pre)=>{
+      let newIndex = pre + 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  const randomPerson = ()=>{
+    let randomPerson = Math.floor(Math.random() * people.length)
+    if(randomPerson === index) {
+      randomPerson = index + 1
+    }
+    setIndex(checkNumber(randomPerson))
+  }
+
+  return <article className='review'>
+    <div className='img-container'>
+      <img src={image} alt={name} className='person-img'></img>
+      <span className='quote-icon'>
+        <FaQuoteRight/>
+      </span>
+    </div>
+
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <div className='div-container'>
+      <button className='prev-btn' onClick={handlePrePerson}>
+        <FaChevronLeft/>
+      </button>
+      <button className='next-btn' onClick={handleNextPerson}>
+        <FaChevronRight/>
+      </button>
+    </div>
+    <button className='random-btn' onClick={randomPerson}>suprise me</button>
+  </article>;
 };
 
 export default Review;
--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,75 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading,setLoading] = useState(true);
+  const [job,setJob] = useState([]);
+  const [value,setValue] = useState(0)
+
+  // async function: chặn thực thi của chương trình cho tới khi nó hoàn tất thao tác và trả ra kết quả
+  const fetchJobs = async () =>{
+    const response = await fetch(url);
+    // khi gặp await trong hàm bất đồng bộ async, chương trình sẽ tạm dừng thực thi hàm đó và cho phếp
+    // các tác vụ khác tiếp tục thực hiện. Khi await được hoàn thành,chương trình sẽ tiếp tục thi hàm bất
+    // đồng bộ từ nơi nó đã tạm dừng trước đó
+    const newJobs = await response.json()
+    setJob(newJobs)
+    setLoading(false)
+  };
+   
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if(loading){
+    return (
+    <section className='section-loading'>
+      <h1>Loading...</h1>
+    </section>
+    )
+  }
+
+  const {company,dates,duties,title} = job[value]
+
+  return <section className='section'>
+    <div className='title'>
+      <h2>experience</h2>
+      <div className='underline'></div>
+    </div>
+
+    <div className='job-center'>
+      {/* btn container */}
+      <div className='btn-container'>
+        {
+          job.map((item,index)=>{
+            return <button key={item.id}
+              onClick={()=>setValue(index)}
+              className={`job-btn ${index === value &&
+              'active-btn'
+            }`}            
+            >
+              {item.company}
+            </button>
+          })
+        }
+      </div>
+      {/* job info */}
+      <article className='job-info'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty,index)=>{
+          return(
+            <div key={index} className='job-desc'>
+              <FaAngleDoubleRight
+                className='job-icon'
+              />
+              <p>{duty}</p>
+            </div>
+          )
+        })}
+      </article>
+    </div>
+  </section>
 }
 
 export default App
