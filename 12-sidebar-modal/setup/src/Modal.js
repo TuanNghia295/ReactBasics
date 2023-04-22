03c3ecd4b74802654485ffed110c3f7cabf3b397
TuanNghia295 <tuannghiait2905@gmail.com>
2023-04-22 14:04:22 +0700

    Sidebar-modal,custom Hook ,useContext

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useMemo,useCallback } from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
 const Modal = () => {
-  return <h2>Modal</h2>
+  const {isModalOpen,closeModal} = useGlobalContext()
+
+  const modalClass = useMemo(() => {
+    return `${isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}`
+  }, [isModalOpen])
+
+  const handleCloseModal = useCallback(()=>{
+    closeModal();
+  },[closeModal])
+
+  return <div className={modalClass}>
+    <div className='modal-container'>
+      <h3>Modal content</h3>
+      <button className='close-modal-btn'onClick={()=>handleCloseModal()}>
+        <FaTimes/>
+      </button>
+    </div>
+  </div>
 }
 
 export default Modal
