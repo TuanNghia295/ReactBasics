c9bc420eb13c2bd9f1ba2784178e7fd6a0da8745
TuanNghia295 <tuannghiait2905@gmail.com>
2023-05-05 00:09:58 +0700

    using Cocktails API,fetch API, React-router-dom

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -5,7 +5,54 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading,setLoading] = useState(false)
+  const [searchTerm,setSearchTerm] = useState('a')
+  const [cocktails,setcocktails] = useState([])
+
+  const fetchDrinks = useCallback(async () =>{
+    setLoading(true)
+    try {
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      const {drinks} = data
+      if(drinks){
+        const newCocktails = drinks.map((item)=>{
+          const {idDrink,strDrink,strDrinkThumb,strAlcoholic,strGlass} = item
+          return{
+            id:idDrink,
+            name:strDrink,
+            image:strDrinkThumb,
+            info:strAlcoholic,
+            glass:strGlass
+          }
+        })
+        setcocktails(newCocktails)
+      }else{
+        setcocktails([])
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+
+    }
+  },[searchTerm])
+
+  useEffect(() =>{
+    fetchDrinks()
+  },[searchTerm])
+
+  return (
+  <AppContext.Provider value={
+    {
+      loading,
+      searchTerm,
+      cocktails,
+      setSearchTerm
+    }
+  }>
+    {children}
+  </AppContext.Provider>)
 }
 // make sure use
 export const useGlobalContext = () => {
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Welcome to the wonderful world of technology! 
+        In this ever-changing landscape, staying up-to-date with 
+        the latest advancements can be a challenge. 
+        That's why we're here to guide you through the latest trends 
+        and innovations in the tech industry. 
+        From artificial intelligence and blockchain 
+        to cloud computing and cybersecurity, we've got you covered. 
+        Whether you're a seasoned tech enthusiast or just starting out, 
+        there's something for everyone here. So come on in, join the conversation, 
+        and let's explore the exciting world of technology together!
+      </p>
+    </section>
   )
 }
 
